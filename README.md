f36f7a29dadba4553d23b40390f0e1709deb26a7
Andres <an.rondon@gmail.com>
2020-08-09 22:27:08 -0400

    Cleaning files for release

--- README.md
+++
@@ -1,2 +0,0 @@
-# kiro-rmvx
-Kiro
--- Scripts/Custom/Wora's Scripts/Gradient Bug Fix.rb
+++
@@ -1,8 +0,0 @@
-# Fix the gradient_fill_rect method bug # by Woratana
-class Bitmap
-  alias wora_bug_bmp_gfr gradient_fill_rect unless method_defined?('wora_bug_bmp_gfr')
-  def gradient_fill_rect(*args)
-    args.pop if args.size == 7 and !args.last
-    wora_bug_bmp_gfr(*args)
-  end
-end
